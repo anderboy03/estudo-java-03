4ded6c04d980f8608836c74c0c2c92e445a69f73
Anderson Clayton Ribas <andersex03@gmail.com>
2024-09-05 11:23:06 -0300

    Add files via upload

---
+++ Funcao001/src/funcao001/Funcao001.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package funcao001;
+
+/**
+ *
+ * @author User
+ */
+public class Funcao001 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Fatorial f =new Fatorial();
+        f.setValor(5);
+        System.out.println(f.getFatorial());
+    }
+    
+}
