3b557fb674dfe26782ea521a30ed69e1680957c6
Anderson Clayton Ribas <andersex03@gmail.com>
2024-09-05 10:17:09 -0300

    exercicios metodos e funcões

---
+++ TestFuncao02/src/testfuncao02/Operacao.java
@@ -0,0 +1,18 @@
+
+package testfuncao02;
+
+
+
+public class Operacao {
+    public  static String contador(int i, int f){
+        String s = "";
+        for(int c = i; c<=f; c++){
+            s +=c + "";
+        }
+        return s; 
+    }
+   
+        
+    }
+    
+
---
+++ TesteFuncao1/src/testefuncao1/TesteFuncao1.java
@@ -0,0 +1,20 @@
+
+package testefuncao1;
+
+
+public class TesteFuncao1 {
+     static int soma(int a, int b){
+        int s = a+b;
+        return s;
+    }
+
+    
+    public static void main(String[] args) {
+        System.out.println("Começou o programa");
+        int sm = soma(5,2);
+        System.out.println("A soma do valor " + sm);
+        
+        
+    }
+    
+}
